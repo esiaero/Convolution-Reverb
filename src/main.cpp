f92d52168a94df1dd9575c14dfef1685beebd1b2
esiaero <anthias@seas.upenn.edu>
2023-03-17 10:38:38 -0400

    iterative ver

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include <complex>
 #include <bit>
 #include <chrono>
-
 #include <cuda_runtime_api.h>
+
 #include "AudioFile.h"
 
 bool checkGPUAvailable() {
@@ -39,31 +39,71 @@ bool checkGPUAvailable() {
 //    }
 //}
 
+// Recursive, in-place
+//void fft(std::vector<std::complex<double>>& signal) {
+//    using namespace std::complex_literals;
+//
+//    if (signal.size() <= 1) {
+//        return;
+//    }
+//    else if ((signal.size() & (signal.size() - 1)) != 0) {
+//        std::cout << "Size was " << signal.size() << ", needs to be a pow2" << std::endl;
+//    }
+//    else {
+//        std::vector<std::complex<double>> even, odd;
+//        even.reserve(signal.size() / 2);
+//        odd.reserve(signal.size() / 2);
+//        for (size_t i = 0; i < signal.size(); ) {
+//            even.push_back(signal.at(i++));
+//            odd.push_back(signal.at(i++));
+//        }
+//        fft(even);
+//        fft(odd);
+//
+//        for (std::size_t i = 0; i < signal.size() / 2; ++i) {
+//            std::complex<double> val = std::exp(-2. * 1i * M_PI * (double)i / (double)signal.size()) * odd.at(i);
+//
+//            signal.at(i) = even.at(i) + val;
+//            signal.at(i + signal.size() / 2) = even.at(i) - val;
+//        }
+//    }
+//}
+
+// Breadth-first implementation, O(1) storage, ~ 1/2 runtime of recusrive
 void fft(std::vector<std::complex<double>>& signal) {
-    using namespace std::complex_literals;
+    // Bit reverse permutation... kind of scuffed
+    // http://graphics.stanford.edu/~seander/bithacks.html
+    int log2len = (int)(round(std::log2(signal.size())));
+    for (uint32_t a = 0; a < signal.size(); ++a) {
+        uint32_t b = a;
+        // Swap in groups of 1, 2, 4, 8, 16...
+        b = (((b & 0XAAAAAAAA) >> 1) | ((b & 0x55555555) << 1));
+        b = (((b & 0XCCCCCCCC) >> 2) | ((b & 0x33333333) << 2));
+        b = (((b & 0XF0F0F0F0) >> 4) | ((b & 0x0F0F0F0F) << 4));
+        b = (((b & 0xFF00FF00) >> 8) | ((b & 0x00FF00FF) << 8));
+        b = ((b >> 16) | (b << 16)) >> (32 - log2len);
 
-    if (signal.size() <= 1) {
-        return;
-    }
-    else if ((signal.size() & (signal.size() - 1)) != 0) {
-        std::cout << "Size was " << signal.size() << ", needs to be a pow2" << std::endl;
-    }
-    else {
-        std::vector<std::complex<double>> even, odd;
-        even.reserve(signal.size() / 2);
-        odd.reserve(signal.size() / 2);
-        for (size_t i = 0; i < signal.size(); ) {
-            even.push_back(signal.at(i++));
-            odd.push_back(signal.at(i++));
+        if (b > a) {
+            std::iter_swap(signal.begin() + a, signal.begin() + b);
         }
-        fft(even);
-        fft(odd);
+    }
 
-        for (std::size_t i = 0; i < signal.size() / 2; ++i) {
-            std::complex<double> val = std::exp(-2. * 1i * M_PI * (double)i / (double)signal.size()) * odd.at(i);
+    for (int m = 2; m <= signal.size(); m <<= 1) {
+        for (int k = 0; k < signal.size(); k += m) {
+            for (int j = 0; j < m / 2; ++j) {
+                std::complex<double> even = signal.at(k + j);
+                std::complex<double> odd = signal.at(k + j + m / 2);
+                
+                double term = -2. * M_PI * (double)j / (double)m;
+                std::complex<double> val(cos(term), sin(term));
+                val *= odd;
+                // Below is minutely slower? Uncertain
+                // using namespace std::complex_literals;
+                // std::complex<double> val = std::exp(-2. * 1i * M_PI * (double)j / (double)m) * odd;
 
-            signal.at(i) = even.at(i) + val;
-            signal.at(i + signal.size() / 2) = even.at(i) - val;
+                signal.at(k + j) = even + val;
+                signal.at(k + j + m / 2) = even - val;
+            }
         }
     }
 }
@@ -87,25 +127,26 @@ void convolution(std::vector<double> &a, std::vector<double> &b, std::vector<dou
     }
 }
 
-void fftConvolution(std::vector<double> &a, std::vector<double> &dry, std::vector<double> &out, double wetGain) {
-    size_t padded = std::bit_ceil(a.size() + dry.size() - 1); 
+void fftConvolution(std::vector<double> &dry, std::vector<double> &ir, std::vector<double> &out, double wetGain) {
+    size_t padded = std::bit_ceil(ir.size() + dry.size() - 1);
     out.resize(padded);
 
     // TODO: less clumsy way to handle this conversion? might need to change audio library
-    std::vector<std::complex<double>> complexIR;
-    complexIR.reserve(padded);
-    std::transform(a.cbegin(), a.cend(), std::back_inserter(complexIR),
-        [](double r) { return std::complex<double>(r); });
-    for (size_t i = a.size(); i < padded; ++i) {
-        complexIR.push_back(std::complex(0.));
-    }
-
+    // reinterpret cast may be possible
     std::vector<std::complex<double>> complexDry;
     complexDry.reserve(padded);
     std::transform(dry.cbegin(), dry.cend(), std::back_inserter(complexDry),
         [](double r) { return std::complex<double>(r); });
     for (size_t i = dry.size(); i < padded; ++i) {
         complexDry.push_back(std::complex(0.));
+    }
+
+    std::vector<std::complex<double>> complexIR;
+    complexIR.reserve(padded);
+    std::transform(ir.cbegin(), ir.cend(), std::back_inserter(complexIR),
+        [](double r) { return std::complex<double>(r); });
+    for (size_t i = ir.size(); i < padded; ++i) {
+        complexIR.push_back(std::complex(0.));
     }
 
     fft(complexIR);
@@ -134,17 +175,18 @@ void fftConvolution(std::vector<double> &a, std::vector<double> &dry, std::vecto
 
 int main(int argc, char* argv[]) {
     AudioFile<double> ir, dry;
+    AudioFile<double>::AudioBuffer buffer;
     int channel = 0;
+    double WET_GAIN = 0.115f;
+    std::string dryPath = "./samples/aperture_dry.wav";
+    std::string irPath = "./samples/dales_ir.wav";
+
     std::string outputNaive = "./samples/naiveConvolved.wav";
-    std::string outputFilePath = "./samples/convolved.wav";
+    std::string outputPath = "./samples/convolved.wav";
 
-    ir.load("./samples/dales_ir.wav");
-    dry.load("./samples/aperture_dry.wav");
-
-    AudioFile<double> convolved;
-    convolved.setBitDepth(dry.getBitDepth());
-    convolved.setSampleRate(dry.getSampleRate());
-    convolved.setNumChannels(dry.getNumChannels());
+    buffer.resize(1);
+    ir.load(irPath);
+    dry.load(dryPath);
 
     //std::cout << "IR Bit Depth: " << ir.getBitDepth() << std::endl;
     //std::cout << "IR Sample Rate: " << ir.getSampleRate() << std::endl;
@@ -158,29 +200,27 @@ int main(int argc, char* argv[]) {
     std::cout << "Dry Length in Seconds: " << dry.getLengthInSeconds() << std::endl;
     std::cout << "Sample count: " << dry.samples[channel].size() << std::endl;
 
-    /////// Naive convolution test
-    //AudioFile<double>::AudioBuffer buffer;
-    //buffer.resize(1);
-    //convolution(ir.samples[channel], dry.samples[channel], buffer[channel]);
-    //convolved.setAudioBuffer(buffer);
-    //convolved.save(outputNaive, AudioFileFormat::Wave);
-    //-----
-
-    AudioFile<double>::AudioBuffer buffer;
-    buffer.resize(1);
-    double WET_GAIN = 0.115f;
-
-    auto start = std::chrono::high_resolution_clock::now();
-    fftConvolution(ir.samples[channel], dry.samples[channel], buffer[channel], WET_GAIN);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
-    std::cout << "Duration: " << duration.count() << " microseconds" << std::endl;
-
-    convolved.setAudioBuffer(buffer);
-    convolved.save(outputFilePath, AudioFileFormat::Wave);
 
     //bool gpuAvailable = checkGPUAvailable();
     //cudaDeviceProp deviceProp;
     //cudaGetDeviceProperties(&deviceProp, gpuDevice);
+
+    // Main operation
+    auto start = std::chrono::high_resolution_clock::now();
+    fftConvolution(dry.samples[channel], ir.samples[channel], buffer[channel], WET_GAIN);
+    auto stop = std::chrono::high_resolution_clock::now();
+    // End main operation
+
+
+    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Duration: " << duration.count() << " microseconds" << std::endl;
+
+    AudioFile<double> convolved;
+    convolved.setBitDepth(dry.getBitDepth());
+    convolved.setSampleRate(dry.getSampleRate());
+    convolved.setNumChannels(dry.getNumChannels());
+    convolved.setAudioBuffer(buffer);
+    convolved.save(outputPath, AudioFileFormat::Wave);
+
     return 0;
 }
